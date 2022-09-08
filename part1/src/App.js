e1c7fc56e2b85dacc89ca1949065782564037c3f
zhengtian <tangzt0415@gmail.com>
2022-09-09 00:34:30 +0800

    part1 edits

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,14 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <span>hello my name is {props.poop} </span>
-      </header>
-    </div>
-  );
+function Header(props){
+  return(  <h1>{props.course}</h1>
+  )
 }
 
-export default App;
+function Content(props){
+  return(
+    <div>
+    <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+      </div>
+  )
+}
+
+function Total(props){
+  return(
+  <p>Number of exercises {props.total}</p>)
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content part3  = {part3} part1 = {part1} part2 = {part2} exercises1= {exercises1} exercises2= {exercises2} exercises3 = {exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
