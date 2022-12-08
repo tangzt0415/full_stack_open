658f540fae288f77e7f9656e23ddcefcbed2613b
ZhengTian <tangzt96@gmail.com>
2022-12-09 02:41:26 +0800

    1c.) function definition outside of jsx

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,29 +4,34 @@ const Display = (props) => {
   return <div>{props.counter}</div>
 }
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>
-}
+// const Button = (props) => {
+//   return <button onClick={props.onClick}>{props.text}</button>
+// }
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-
+  //function definition outside of jsx for button function -> Good practise
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
   return (
     <div>
       <Display counter={counter} />
       <div>
         <Button
-          onClick={() => {
-            setCounter(counter + 1)
-          }}
+          onClick={increaseByOne}
           text="increase"
         />
         <Button
-          onClick={() => {
-            setCounter(0)
-          }}
+          onClick={setToZero}
           text="reset"
         />
+        <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />  
       </div>
     </div>
   )
